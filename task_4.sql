0e7e702c8aeb4e7d48f3105131d4391f6bc2daab
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-07-29 17:03:14 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,15 @@
+-- Query the INFORMATION_SCHEMA to get details about the columns in the Books table
+SELECT 
+    COLUMN_NAME AS "Column Name",
+    COLUMN_TYPE AS "Column Type",
+    IS_NULLABLE AS "Is Nullable",
+    COLUMN_DEFAULT AS "Default Value",
+    EXTRA AS "Extra Info"
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
 
