02bef469f1aabdaff08ca54980dab9bcbf636f0d
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-07-29 17:01:24 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,6 @@
 -- Show all tables in the currently selected database
 SHOW TABLES;
 mysql -u your_username -p -D alx_book_store < task_3.sql
+mysql -u your_username -p < task_3.sql
+mysql -u your_username -p -D your_database_name < task_3.sql
 
