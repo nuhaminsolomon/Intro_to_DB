dbbc0422352dd477e84ccc3d9f76b644c2fda090
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 11:34:39 +0300

    Update task_6.sql
    n

--- task_6.sql
+++ task_6.sql
@@ -4,13 +4,3 @@ INSERT INTO customer (customer_id, customer_name, email, address)
  VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
         (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
         (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'Books'
-ORDER BY 
-    ORDINAL_POSITION;
-   
-
-   
