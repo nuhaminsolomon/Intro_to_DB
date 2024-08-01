fcf4ccba4f62f694717ce8b52b620d49ce9f2f66
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 10:22:29 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -4,4 +4,6 @@ INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+mysql -u your_username -p alx_book_store < task_6.sql
+
 
