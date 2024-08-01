e01294b67676f90805ee756a3abc96f32e091bec
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 10:38:52 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,8 @@
 -- task_6.sql
-USE alx_book_store;
+-- This script inserts multiple rows into the 'customer' table.
+
+-- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-mysql -u your_username -p alx_book_store < task_6.sql
-
-
