9c4343288e718fe0a08e826b87aca4bacd43d93e
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 11:24:46 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -4,8 +4,13 @@ INSERT INTO customer (customer_id, customer_name, email, address)
  VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
         (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
         (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-   mysql -u <root> -p alx_book_store
-   SOURCE /alx_book_store/to/your/insert_customers.sql;
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
    
 
    
