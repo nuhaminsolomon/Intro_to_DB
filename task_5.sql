9fb6b6eaa1efb3fbd4b96440ca0812aa96b4648c
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 10:57:11 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,3 @@
--- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-
+sql = "INSERT INTO customer table ( customer_id, customer_name, email, adress) VALUES (%s, %s)"
+val = (1, 'cole Baidoo, 'cbaidoo@sandteech.com', '123 Happiness Ave.')
+mycursor.execute(sql, val)
