cb020eeea62c1afb1c81e66218e742e251b57408
nuhaminsolomon <nuhamin.sele@gmail.com>
2024-08-01 10:43:03 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,18 +1,1 @@
-mysql -u your_username -p
-USE alx_book_store;
-SELECT *
-FROM customer
-WHERE customer_id = 2;
-+-------------+------------------+-----------------------+------------------------+
-| customer_id | customer_name    | email                 | address                |
-+-------------+------------------+-----------------------+------------------------+
-|           2 | Blessing Malik   | bmalik@sandtech.com   | 124 Happiness Ave.     |
-+-------------+------------------+-----------------------+------------------------+
--- verify_insertion.sql
-USE alx_book_store;
-
-SELECT *
-FROM customer
-WHERE customer_id = 2;
-mysql -u your_username -p alx_book_store < verify_insertion.sql
 
